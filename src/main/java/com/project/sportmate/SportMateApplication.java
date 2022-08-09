99a06af422e965750c44193e65647ec819d8348d
이태균 <>
2022-08-09 12:11:28 +0900

    [이태균] SportMate Project 초기 환경설정 셋팅

---
+++ src/main/java/com/project/sportmate/SportMateApplication.java
@@ -0,0 +1,13 @@
+package com.project.sportmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sportmate/SportMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sportmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
