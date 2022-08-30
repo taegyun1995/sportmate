99194710c238d2a125db60b64d6043e1df4e32cf
이태균 <>
2022-08-30 19:48:46 +0900

    [이태균] main project sportmate
    페이지 인터셉터 기능 구현
    
    taegyun1995/sportmate#4

---
+++ src/main/java/com/project/sportmate/config/WebMVCConfig.java
@@ -0,0 +1,35 @@
+package com.project.sportmate.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.project.sportmate.common.FileManagerService;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer{
+	
+	// 특정 경로의 파일을 외부에서 접근 가능하도록
+	// 그 파일의 접근 경로를 잡아준다.
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler("/images/**")
+		.addResourceLocations("file:///" + FileManagerService.IMAGE_UPLOAD_PATH);
+	}
+	
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
+	// 인터셉터 등록
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**") // 어떤 경로(path)가 인터셉터를 거쳐서 수행될지 설정
+		.excludePathPatterns("/static/**", "/images/**", "/user/sign/out");
+	}
+	
+}
